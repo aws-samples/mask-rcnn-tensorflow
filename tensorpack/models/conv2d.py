67b9e2eea6706590ac2afaf41ae2368be85249b3
Fei Wu <fewu@amazon.com>
2019-08-27 18:27:04 -0700

    debug

--- tensorpack/models/conv2d.py
+++ tensorpack/models/conv2d.py
@@ -249,11 +249,11 @@ def Conv2DTranspose(
                               filters]
 
         kernel_shape = shape2d(kernel_size)
-        with tf.variable_scope(tf.get_variable_scope(), custom_getter=float32_variable_storage_getter(dtype=tf.float16)):
-            W = tf.get_variable('W', kernel_shape + [filters, channels_in], initializer=kernel_initializer)
+        with tf.variable_scope(tf.get_variable_scope(), custom_getter=float32_variable_storage_getter):
+            W = tf.get_variable('W', kernel_shape + [filters, channels_in], initializer=kernel_initializer, dtype=tf.float16)
         if use_bias:
-            with tf.variable_scope(tf.get_variable_scope(), custom_getter=float32_variable_storage_getter(dtype=tf.float16)):
-                b = tf.get_variable('b', [filters], initializer=bias_initializer)
+            with tf.variable_scope(tf.get_variable_scope(), custom_getter=float32_variable_storage_getter):
+                b = tf.get_variable('b', [filters], initializer=bias_initializer, dtype=tf.float16)
         conv = tf.nn.conv2d_transpose(
             inputs, W, out_shape_dyn,
             shape4d(strides, data_format=data_format),
