b47d8f432ea13856476e162e84d59a137af09e16
Fei Wu <fewu@amazon.com>
2019-09-24 14:57:00 -0700

    minor change

--- infra/docker_multi_node/run_multinode.sh
+++ infra/docker_multi_node/run_multinode.sh
@@ -10,11 +10,11 @@ hosts=`cat $HOSTS`
 
 for host in $hosts; do
   if [ $host != $MASTER_HOST ]; then
-    ssh $host "nvidia-docker run --network=host -v /mnt/share/ssh:/root/.ssh -v ~/data:/data -v ~/logs:/logs mask-rcnn-tensorflow:dev-${BRANCH_NAME} /mask-rcnn-tensorflow/infra/docker_multi_node/sleep.sh"
+    ssh $host "nvidia-docker run --network=host -v /mnt/share/ssh:/root/.ssh -v ~/data:/data -v ~/logs:/logs mask-rcnn-tensorflow:dev-${BRANCH_NAME} /bin/bash -c '/mask-rcnn-tensorflow/infra/docker_multi_node/sleep.sh'"
   fi
 done
-nvidia-docker run --network=host -v /mnt/share/ssh:/root/.ssh -v ~/data:/data -v ~/logs:/logs mask-rcnn-tensorflow:dev-${BRANCH_NAME} "cp /data/${HOSTS_SLOTS} /mask-rcnn-tensorflow/infra/docker_multi_node/hosts; cd /mask-rcnn-tensorflow/infra/docker_multi_node/; ./train_multinode.sh $NUM_GPU $BS |& tee /logs/${NUM_GPU}x${BS}.log"
-
+nvidia-docker run --network=host -v /mnt/share/ssh:/root/.ssh -v ~/data:/data -v ~/logs:/logs mask-rcnn-tensorflow:dev-multi-node-docker /bin/bash -c "cp /data/${HOSTS_SLOTS} /mask-rcnn-tensorflow/infra/docker_multi_node/hosts; cd /mask-rcnn-tensorflow/infra/docker_multi_node/; ./train_multinode.sh $NUM_GPU $BS |& tee /logs/${NUM_GPU}x${BS}.log"
 for host in $hosts; do
+  echo "stop containers..."
   docker stop $(docker ps -a -q)
 done
