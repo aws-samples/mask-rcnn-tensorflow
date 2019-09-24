d5cf7e6c253a9486b658a4ad4878be76119db74a
Fei Wu <fewu@amazon.com>
2019-09-24 15:51:51 -0700

    minor change

--- infra/docker_multi_node/run_multinode.sh
+++ infra/docker_multi_node/run_multinode.sh
@@ -15,6 +15,9 @@ for host in $hosts; do
 done
 nvidia-docker run --network=host -v /mnt/share/ssh:/root/.ssh -v ~/data:/data -v ~/logs:/logs mask-rcnn-tensorflow:dev-multi-node-docker /bin/bash -c "cp /data/${HOSTS_SLOTS} /mask-rcnn-tensorflow/infra/docker_multi_node/hosts; cd /mask-rcnn-tensorflow/infra/docker_multi_node/; ./train_multinode.sh $NUM_GPU $BS |& tee /logs/${NUM_GPU}x${BS}.log"
 for host in $hosts; do
-  echo "stop containers..."
-  docker stop $(docker ps -a -q)
+  if [ $host != $MASTER_HOST ]; then
+    echo "stop containers and screens..."
+    docker stop $(docker ps -a -q)
+    pkill screen
+  fi
 done
