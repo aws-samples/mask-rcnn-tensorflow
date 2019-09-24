b313b73702aae5bcb900b4e52bf4f7165bb17567
Fei Wu <fewu@amazon.com>
2019-09-24 16:56:14 -0700

    minor change

--- infra/docker_multi_node/run_multinode.sh
+++ infra/docker_multi_node/run_multinode.sh
@@ -10,7 +10,7 @@ hosts=`cat $HOSTS`
 
 for host in $hosts; do
   if [ $host != $MASTER_HOST ]; then
-    ssh $host "screen -L -d -m bash -c \" nvidia-docker run --network=host -v /mnt/share/ssh:/root/.ssh -v ~/data:/data -v ~/logs:/logs mask-rcnn-tensorflow:${BRANCH_NAME} /bin/bash -c /usr/sbin/sshd -p 1234; sleep infinity\""
+    ssh $host screen -L -d -m bash -c "nvidia-docker run --network=host -v /mnt/share/ssh:/root/.ssh -v ~/data:/data -v ~/logs:/logs mask-rcnn-tensorflow:${BRANCH_NAME} /bin/bash -c \" /usr/sbin/sshd -p 1234; sleep infinity \" "
   fi
 done
 nvidia-docker run --network=host -v /mnt/share/ssh:/root/.ssh -v ~/data:/data -v ~/logs:/logs mask-rcnn-tensorflow:dev-multi-node-docker /bin/bash -c "cp /data/${HOSTS_SLOTS} /mask-rcnn-tensorflow/infra/docker_multi_node/hosts; cd /mask-rcnn-tensorflow/infra/docker_multi_node/; ./train_multinode.sh $NUM_GPU $BS |& tee /logs/${NUM_GPU}x${BS}.log"
