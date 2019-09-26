a60d7cd878ad3a51ef612c38a2a139473c472ac5
Fewu <fewu@amazon.com>
2019-09-26 13:44:25 -0700

    add allow train as root

--- infra/docker/train.sh
+++ infra/docker/train.sh
@@ -16,6 +16,7 @@ echo ""
 
 mpirun -np ${NUM_GPU} \
 --H localhost:${NUM_GPU} \
+--allow-run-as-root \
 --mca plm_rsh_no_tree_spawn 1 -bind-to none -map-by slot -mca pml ob1 -mca btl ^openib \
 -mca btl_tcp_if_exclude lo,docker0 \
 -mca btl_vader_single_copy_mechanism none \
