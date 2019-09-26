5a9a3bd64405cf7fdd4f6223e26c6a077378ed42
Fewu <fewu@amazon.com>
2019-09-26 13:49:13 -0700

    simplify the script

--- infra/docker_multi_node/run_multinode.sh
+++ infra/docker_multi_node/run_multinode.sh
@@ -2,8 +2,9 @@ MASTER_HOST=${1:-"127.0.0.1"}
 IMAGE_NAME=${2:-"fewu/mask-rcnn-tensorflow:master-latest"}
 NUM_GPU=${3:-8}
 BS=${4:-4}
+JOB_NAME=${5:-"8x4-run1"}
 HOSTS=${5:-"hosts"}
-HOSTS_SLOTS=${6:-"hosts_slots"}
+HOSTS_SLOTS=${6:-"hosts"}
 
 
 hosts=`cat $HOSTS`
@@ -15,11 +16,11 @@ for host in $hosts; do
     ' /bin/bash -c \" cd /mask-rcnn-tensorflow; git pull; /usr/sbin/sshd -p 1234; sleep infinity \""'
   fi
 done
-nvidia-docker run --network=host -v /mnt/share/ssh:/root/.ssh -v ~/data:/data -v ~/logs:/logs ${IMAGE_NAME} /bin/bash -c "cp /data/${HOSTS_SLOTS} /mask-rcnn-tensorflow/infra/docker_multi_node/hosts; cd /mask-rcnn-tensorflow/infra/docker_multi_node/; git pull; ./train_multinode.sh $NUM_GPU $BS |& tee /logs/${NUM_GPU}x${BS}.log"
+nvidia-docker run --network=host -v /mnt/share/ssh:/root/.ssh -v ~/data:/data -v ~/logs:/logs ${IMAGE_NAME} /bin/bash -c "cp /data/${HOSTS_SLOTS} /mask-rcnn-tensorflow/infra/docker_multi_node/hosts; cd /mask-rcnn-tensorflow/infra/docker_multi_node/; git pull; ./train_multinode.sh $NUM_GPU $BS |& tee /logs/${JOB_NAME}.log"
 
 for host in $hosts; do
   if [ $host != $MASTER_HOST ]; then
-    echo "stop containers and screens..."
+    echo "stop containers and screens on ${host}..."
     ssh $host 'screen -L -d -m bash -c "docker ps -q | xargs -r docker stop; pkill screen"'
   fi
 done
