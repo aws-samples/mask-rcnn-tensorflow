1fc14eca6c4bde0c78e63bd674ba4fb8f3e5cfdd
Ben Snyder <jbsnyder@amazon.com>
2019-11-04 17:15:50 -0800

    updated coco utils

--- notebooks/utils/coco.py
+++ notebooks/utils/coco.py
@@ -70,6 +70,60 @@ class COCOSubsetter(object):
         subset['images'] = [i for i in self.instances['images'] if i['id'] in images]
         return subset
 
+    def create_subset_dir(self, dir):
+        assert not dir.exists(), "directory {} exists".format(dir.as_posix())
+        dir.mkdir()
+        dir.joinpath('annotations').mkdir()
+        dir.joinpath('train2017').mkdir()
+
+    def create_subset(self, images, dir):
+        dir = Path(dir)
+        self.create_subset_dir(dir)
+        for image in images:
+            shutil.copy(self.images[image],
+                        dir.joinpath('train2017').joinpath(os.path.basename(self.images[image])))
+        with open(dir.joinpath('annotations').joinpath('instances_train2017.json'), 'w') as anno_file:
+            anno_file.write(json.dumps(self.filter_subset(images)))
+
+
+class COCODuplicator(object):
+    def __init__(self, data_dir):
+        self.data_dir = Path(data_dir)
+        self.instance_file = self.data_dir.joinpath('annotations/instances_train2017.json')
+        self.train_dir = self.data_dir.joinpath('train2017')
+        self.images = list(self.train_dir.glob('*.jpg'))
+        self.images = {int(os.path.splitext(os.path.basename(i.as_posix()))[0]): i for i in self.images}
+        return
+
+    def load_annotations(self):
+        with open(self.instance_file) as infile:
+            self.instances = json.load(infile)
+
+    def duplicate_annotations(self, count):
+        dup_annotations = dict()
+        dup_annotations['info'] = self.instances['info']
+        dup_annotations['licenses'] = self.instances['licenses']
+        dup_annotations['categories'] = self.instances['categories']
+        new_annotations = []
+        new_images = []
+        for num in range(count):
+            for anno in self.instances['annotations']:
+                anno_copy = anno.copy()
+                anno_copy['image_id'] = int("{}{}".format(anno['image_id'], str(num)))
+                new_annotations.append(anno_copy)
+            for image in self.instances['images']:
+                image_copy = image.copy()
+                filename = os.path.splitext(image_copy['file_name'])
+                image_copy['file_name'] = "{}{}{}".format(filename[0],
+                                                          str(num),
+                                                          filename[1])
+                new_images.append(image_copy)
+        dup_annotations['annotations'] = new_annotations
+        dup_annotations['images'] = new_images
+        return dup_annotations
+
+
+
 
 
 
@@ -78,3 +132,6 @@ coco_loader = COCOSubsetter('/Users/jbsnyder/PycharmProjects/mrcnn-notebooks/dat
 coco_loader.load_annotations()
 images = coco_loader.random_subset(10)
 coco_loader.filter_subset(list(images.keys()))
+
+coco_dup = COCODuplicator('/Users/jbsnyder/PycharmProjects/mrcnn-notebooks/subset')
+
